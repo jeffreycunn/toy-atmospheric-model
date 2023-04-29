4e0822877ffebd026d35a08d60be99ca87fcebb0
tnayagarwal <tanaynilesh@gmail.com>
2023-04-29 10:54:36 +0530

    Made all functions julia friendly

--- diffusion.jl
+++ diffusion.jl
@@ -1,33 +1,25 @@
-function [ varp,varm ] = diffusion(varb,varp,varm,cmixh,cmixv,dx,dz,dt,nx,nz)
-%UNTITLED9 Summary of this function goes here
-%   Detailed explanation goes here
+#this code contained functions from mathlab so i added julia friendly functions
+function diffusion!(varp, varm, varb, cmixh, cmixv, dx, dz, dt, nx, nz)
+  # Horizontal Diffusion
+  kmixh = cmixh * dx^2 / dt
+  @inbounds for k = 2:nz-1
+      @inbounds for i = 2:nx-1
+          varp[i,k] += 2.0 * dt * kmixh * (varm[i+1,k] - 2.0 * varm[i,k] + varm[i-1,k]) / dx^2
+      end
+  end
 
-% Horizontal Diffusion
-
-kmixh=cmixh*dx*dx/dt;
-
-for k = 2 : nz-1
-  for i = 2 : nx-1
-varp[i,k] = varp[i,k] + 2.0*dt*kmixh*(varm[i+1,k]-2.0*varm[i,k]+varm[i-1,k])/(dx*dx);
-
-
+  # Vertical Diffusion
+  kmixv = cmixv * dz^2 / dt
+  @inbounds for k = 3:nz-1
+      @inbounds for i = 2:nx-1
+          varp[i,k] += 2.0 * dt * kmixv * ((varm[i,k+1] - varb[k+1])
+                                           - 2.0 * (varm[i,k] - varb[k])
+                                           + (varm[i,k-1] - varb[k-1])) / dz^2
+      end
   end
 end
+#= Note that I've made the following changes:
 
-% Vertical Diffusion
-kmixv=cmixv*dz*dz/dt;
-
-for k = 3 : nz-1
-  for i = 2 : nx-1
-
-varp(i,k) = varp[i,k] + 2*dt*(kmixv*((varm[i,k+1]-varb[k+1])     ...
-                 -2.0*(varm[i,k]-varb[k])                        ...
-                 +(varm[i,k-1]-varb[k-1]) )/(dz*dz));
-
-
-
-  end
-end
-
-
-end
+Added a ! to the function name to indicate that it modifies its input variables in place.
+Removed the output variables varp and varm from the function signature since they are modified in place.
+Used Julia's @inbounds macro to indicate that it's safe to use indices that are out of bounds, in order to potentially improve performance.=#
